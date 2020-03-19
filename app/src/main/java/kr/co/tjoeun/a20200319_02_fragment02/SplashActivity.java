ce52f5808588a17e4dd1e1087de5c5a23dba4d8a
potential20 <passion212@nate.com>
2020-03-19 13:07:06 +0900

    Handler를 이용한 작업 지연 실행  - 2초 후에 메인화면으로 이동!

--- app/src/main/java/kr/co/tjoeun/a20200319_02_fragment02/SplashActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_02_fragment02/SplashActivity.java
@@ -3,7 +3,9 @@ package kr.co.tjoeun.a20200319_02_fragment02;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import kr.co.tjoeun.a20200319_02_fragment02.databinding.ActivitySplashBinding;
 
@@ -26,6 +28,20 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void setupValues() {
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(mContext, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+
+            }
+        },2000);
+
 
     }
 }
