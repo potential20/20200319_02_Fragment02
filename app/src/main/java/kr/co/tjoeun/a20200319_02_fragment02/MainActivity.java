b9e2f09738d100d496059382dc06de8691e3c18e
potential20 <passion212@nate.com>
2020-03-19 17:07:09 +0900

    어댑터 객체 생성 / 뷰페이저아 연결  => 두개의 페이지가 생성됨.

--- app/src/main/java/kr/co/tjoeun/a20200319_02_fragment02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_02_fragment02/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.tjoeun.a20200319_02_fragment02.adapters.MainViewPagerAdapter;
 import kr.co.tjoeun.a20200319_02_fragment02.databinding.ActivityMainBinding;
 
 //프로젝트 기본 세팅
 //trello => 코드정리 요령 => 3번 내용 따라하기
 public class MainActivity extends BaseActivity {
+    MainViewPagerAdapter mvpa;
     ActivityMainBinding binding = null;
 
     @Override
@@ -27,6 +29,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupValues() {
+        mvpa = new MainViewPagerAdapter(getSupportFragmentManager());
+
+        binding.mainViewPager.setAdapter(mvpa);
 
     }
 }
