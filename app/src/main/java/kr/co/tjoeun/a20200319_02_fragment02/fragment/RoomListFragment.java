a01d311a2529271183f1b2e5ad3353fd920ac7fd
potential20 <passion212@nate.com>
2020-03-19 14:40:54 +0900

    방 목록 프래그먼트 생성

---
+++ app/src/main/java/kr/co/tjoeun/a20200319_02_fragment02/fragment/RoomListFragment.java
@@ -0,0 +1,34 @@
+package kr.co.tjoeun.a20200319_02_fragment02.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import kr.co.tjoeun.a20200319_02_fragment02.R;
+import kr.co.tjoeun.a20200319_02_fragment02.databinding.FragmentRoomListBinding;
+
+public class RoomListFragment extends Fragment {
+
+    FragmentRoomListBinding binding = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_room_list,container,false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//                프래그먼트의 동작 코드 => 리스트뷰 띄워주기.
+    }
+}
